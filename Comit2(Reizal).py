4d2a6db2fd810aa70d11174e7ebff116a3d7fada
schraizel01 <135137581+schraizel01@users.noreply.github.com>
2024-10-03 12:36:17 +0700

    Comit2(Reizal).py
    Fungsi pilihan buku

---
+++ Comit2(Reizal).py
@@ -0,0 +1,9 @@
+def pilihan_buku(self):
+    print("Matematika           = Rp90.000")
+    print("Fisika               = Rp80.000")
+    print("Kalkulus             = Rp85.000")
+    print("Aljabar              = Rp80.000")
+    print("kimia                = Rp90.000")
+    print("Elektronika Dasar    = Rp85.000")
+
+    print("==================================================")
