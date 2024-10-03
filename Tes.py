76bedf65f82ce575edae9507214c08f53bdd219c
Siomayy13 <maytriastuti0.0@gmail.com>
2024-10-03 12:38:43 +0700

    coba coba

--- Tes.py
+++ Tes.py
@@ -1,1 +1,12 @@
 print("Halo Gais")
+
+class PembelianBuku:
+    def __init__(self, buku, harga):
+        self.buku = buku
+        self.harga = harga
+        self.status = ""
+        self.nama = ""
+
+    def IdentitasPembeli(self):
+        self.nama = input("Masukan Nama Anda: ")
+        self.status = input("Anda Berasal dari Prodi Teknik Apa?: ")
