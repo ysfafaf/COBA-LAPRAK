f0b6939be3abe649cb7efdda08d5768f8830bff7
schraizel01 <135137581+schraizel01@users.noreply.github.com>
2024-10-03 13:26:12 +0700

    Update and rename Comit1(Reizal).py to Comit1.py

--- Comit1(Reizal).py
+++
@@ -1,5 +0,0 @@
-def indentitas_pembeli(self):
-    self.nama = input("masukan nama : ")
-    self.status = input("Anda Berasal dari prodi apa? : ")
- print("Haloo zal")
-j jk jkjiii
---
+++ Comit1.py
@@ -0,0 +1,12 @@
+def indentitas_pembeli(self):
+    self.nama = input("masukan nama : ")
+    self.status = input("Anda Berasal dari prodi apa? : ")
+def pilihan_buku(self):
+    print("Matematika           = Rp90.000")
+    print("Fisika               = Rp80.000")
+    print("Kalkulus             = Rp85.000")
+    print("Aljabar              = Rp80.000")
+    print("kimia                = Rp90.000")
+    print("Elektronika Dasar    = Rp85.000")
+
+    print("==================================================")
