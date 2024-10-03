926be95a664f6defeb9c0f4a7cdedb179df3ed51
Siomayy13 <maytriastuti0.0@gmail.com>
2024-10-03 12:39:36 +0700

    coba coba2

--- Tes.py
+++ Tes.py
@@ -10,3 +10,22 @@ class PembelianBuku:
     def IdentitasPembeli(self):
         self.nama = input("Masukan Nama Anda: ")
         self.status = input("Anda Berasal dari Prodi Teknik Apa?: ")
+
+def PilihanBuku(self):
+        print("=============================================")
+
+        print("Matematika = Rp90.000")
+        print("Fisika     = Rp80.000")
+        print("Kalkulus   = Rp85.000")
+        print("Aljabar    = Rp80.000")
+        print("Kimia      = Rp90.000")
+        print("Eldas      = Rp85.000")
+
+        print("=============================================")
+
+
+    def RekomendasiBuku(self):
+        Komputer = "Kalkulus, Aljabar"
+        Elektro = "Kimia, Eldas"
+        Sipil = "Fisika, Matematika"
+        Arsitektur = "Fisika, Matematika"
